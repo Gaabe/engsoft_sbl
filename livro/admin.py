c15161536475c31a3a85d4dd95c4bbbb7339fbe8
Felipe Almeida <filipinas.3@gmail.com>
2018-07-25 18:04:01 -0300

    codigo inicial do sprint2

--- livro/admin.py
+++ livro/admin.py
@@ -1,11 +1,53 @@
 from django.contrib import admin
-from .models import Livro, Indicacao
+from .models import Livro, Indicacao, Exemplar
+
+def comprar_exemplar(modeladmin, request, queryset):
+    single = queryset.first()._meta.verbose_name
+    mult = queryset.first()._meta.verbose_name_plural
+    count = 0
+    for exemplar in queryset:
+        exemplar.vendido = False
+        exemplar.em_venda = False
+    msg_name = mult if count > 1 else single
+    messages.success(request, "Comprado {} {} selecionado{} com sucesso.".format(count, msg_name,
+                        's' if count > 1 else ''))
+comprar_exemplar.short_description = "Comprar os exemplares selecionados"
+
+def criar_exemplar(modeladmin, request, queryset):
+    single = queryset.first()._meta.verbose_name
+    for livro in queryset:
+        Exemplar.objects.create(livro = livro)
+        break # somente o primeiro => resolver para retirar multiselect
+    messages.success(request, "Criado 1 exemplar com sucesso")
+criar_exemplar.short_description = "Criar um exemplar do modelo selecionado"
 
 class LivroAdmin(admin.ModelAdmin):
-	list_display = ("isbn", "titulo", "autor", "editora", "edicao", "ano_de_publicacao")
+    list_display = ("isbn", "titulo", "autor", "editora", "edicao", "ano_de_publicacao")
+    actions = [criar_exemplar]
 
 class IndicacaoAdmin(admin.ModelAdmin):
-	list_display = ("livro", "escola", "ano", "serie", "tipo", "nivel", "preco_sugerido")
+    list_display = ("livro", "escola", "ano", "serie", "tipo", "nivel", "preco_sugerido")
+
+def deactive_selected(modeladmin, request, queryset):
+    single = queryset.first()._meta.verbose_name
+    mult = queryset.first()._meta.verbose_name_plural
+    count = 0
+    for user in queryset:
+        user.is_active = False
+        user.save()
+        count = count + 1
+    msg_name = mult if count > 1 else single
+    messages.success(request, "Desativado {} {} selecionado{} com sucesso.".format(count, msg_name,
+                     's' if count > 1 else ''))
+deactive_selected.short_description = "Desativar os Usuários selecionados"
+
+class ExemplarAdmin(admin.ModelAdmin):
+    list_display = ['livro_titulo', 'livro_autor', 'livro_editora', 'estado', 'em_venda', 'vendido']
+    list_filter = ('estado', 'em_venda', 'vendido',)
+    search_fields = ('livro__titulo', 'livro__autor', 'livro__editora')
+    fields = ("__all__",)
+    actions = [comprar_exemplar]
 
 admin.site.register(Livro, LivroAdmin)
 admin.site.register(Indicacao, IndicacaoAdmin)
+admin.site.register(Exemplar, ExemplarAdmin)
--- livro/models.py
+++ livro/models.py
@@ -7,35 +7,71 @@ TIPO_CHOICES = ((1, "Didático"), (2, "Paradidático"))
 
 class Livro(models.Model):
 
-	class Meta:
-		verbose_name = 'Livro'
-		verbose_name_plural = 'Livros'
-		app_label = "livro"
+    class Meta:
+        verbose_name = 'Livro'
+        verbose_name_plural = 'Livros'
+        app_label = "livro"
 
-	isbn = models.CharField(max_length=200)
-	titulo = models.CharField(max_length=200, verbose_name="Título")
-	autor = models.CharField(max_length=200)
-	editora = models.CharField(max_length=200)
-	edicao = models.CharField(max_length=200, verbose_name="Edição")
-	ano_de_publicacao = models.CharField(max_length=200, verbose_name="Ano de publicação")
+    isbn = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200, verbose_name="Título")
+    autor = models.CharField(max_length=200)
+    editora = models.CharField(max_length=200)
+    edicao = models.CharField(max_length=200, verbose_name="Edição")
+    ano_de_publicacao = models.CharField(max_length=200, verbose_name="Ano de publicação")
 
-	def __str__(self):
-		return "{isbn} - {titulo}".format(isbn=self.isbn, titulo=self.titulo)
+    def __str__(self):
+        return "{isbn} - {titulo}".format(isbn=self.isbn, titulo=self.titulo)
+
+class Exemplar(models.Model):
+
+    class Meta:
+        verbose_name = "Exemplar"
+        verbose_name_plural = 'Exemplares'
+        app_label = "livro"
+
+    RUIM = 0
+    BOM = 1
+    OTIMO = 2
+    ESTADO_TIPO = (
+        (RUIM, 'Ruim'),
+        (BOM, 'Bom/Razoável'),
+        (OTIMO, 'Ótimo')
+    )
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    estado = models.IntegerField('Tipo', choices=ESTADO_TIPO, default=BOM)
+    em_venda = models.BooleanField('Disponível', default=True)
+    vendido = models.BooleanField('Disponível', default=False)
+
+    def livro_titulo(self):
+        return self.livro.titulo
+    livro_titulo.allow_tags = True
+    livro_titulo.short_description = 'Título'
+
+    def livro_autor(self):
+        return self.livro.autor
+    livro_autor.allow_tags = True
+    livro_autor.short_description = 'Autor'
+
+    def livro_editora(self):
+        return self.livro.editora
+    livro_editora.allow_tags = True
+    livro_editora.short_description = 'Editora'
+
 
 class Indicacao(models.Model):
 
-	class Meta:
-		verbose_name = 'Indicação'
-		verbose_name_plural = 'Indicações'
-		app_label = "livro"
+    class Meta:
+        verbose_name = 'Indicação'
+        verbose_name_plural = 'Indicações'
+        app_label = "livro"
 
-	escola = models.ForeignKey(Escola, on_delete=models.CASCADE)
-	ano = models.CharField(max_length=4)
-	serie = models.IntegerField(choices=SERIE_CHOICES, verbose_name="Série")
-	nivel = models.IntegerField(choices=NIVEL_CHOICES, verbose_name="Nível")
-	tipo = models.IntegerField(choices=TIPO_CHOICES)
-	preco_sugerido = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Preço sugerido")
-	livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    escola = models.ForeignKey(Escola, on_delete=models.CASCADE)
+    ano = models.CharField(max_length=4)
+    serie = models.IntegerField(choices=SERIE_CHOICES, verbose_name="Série")
+    nivel = models.IntegerField(choices=NIVEL_CHOICES, verbose_name="Nível")
+    tipo = models.IntegerField(choices=TIPO_CHOICES)
+    preco_sugerido = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Preço sugerido")
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.livro
+    def __str__(self):
+        return self.livro
